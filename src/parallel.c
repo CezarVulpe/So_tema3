c1f668df7f8e437f1fee6704083172b10a079579
cezar_andrei.vulpe <cezar_andrei.vulpe@stud.acs.upb.ro>
2023-12-06 01:20:58 +0200

    bababa

--- src/parallel.c
+++ src/parallel.c
@@ -73,9 +73,22 @@ int main(int argc, char *argv[])
 	tp->oprire=0;
 	list_init(&(tp->head));
 	for (unsigned int i = 0; i < graph->num_nodes; i++) {
-		if (graph->visited[i] == 0) {
-			os_task_t *task = create_task((void *)process_node, (void *)(intptr_t)i, NULL);
-			enqueue_task(tp, task);
+		if (graph->visited[i] == 0)
+		{
+			if (i != 0)
+			{
+				if (graph->nodes[i]->num_neighbours != 0)
+				{
+					os_task_t *task = create_task((void *)process_node, (void *)(intptr_t)i, NULL);
+					enqueue_task(tp, task);
+				}
+			}
+			else
+			{
+				os_task_t *task = create_task((void *)process_node, (void *)(intptr_t)i, NULL);
+				enqueue_task(tp, task);
+				
+			}
 		}
 	}
 	wait_for_completion(tp);
